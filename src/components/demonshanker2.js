8a9849ba0a648dc7312011564df35a8f0d430d74
RDFurman <randydfurman@gmail.com>
2023-11-15 21:27:21 -0700

    Add a loading indicator for unity game

--- src/components/demonshanker2.js
+++ src/components/demonshanker2.js
@@ -5,12 +5,13 @@ import styled from "styled-components";
 export default function DemonShanker2() {
   const [showUnity, setShowUnity] = useState(false);
 
-  const { unityProvider, isLoaded, unload } = useUnityContext({
-    codeUrl: "/demonshanker2/Build/demonshanker2.wasm",
-    frameworkUrl: "/demonshanker2/Build/demonshanker2.framework.js",
-    dataUrl: "/demonshanker2/Build/demonshanker2.data",
-    loaderUrl: "/demonshanker2/Build/demonshanker2.loader.js",
-  });
+  const { unityProvider, loadingProgression, isLoaded, unload } =
+    useUnityContext({
+      codeUrl: "/demonshanker2/Build/demonshanker2.wasm",
+      frameworkUrl: "/demonshanker2/Build/demonshanker2.framework.js",
+      dataUrl: "/demonshanker2/Build/demonshanker2.data",
+      loaderUrl: "/demonshanker2/Build/demonshanker2.loader.js",
+    });
 
   useEffect(() => {
     return async () => {
@@ -21,26 +22,60 @@ export default function DemonShanker2() {
   }, [isLoaded, unload]);
 
   return (
-    <div
-      style={{
-        backgroundColor: "gray",
-        height: "680px",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <GameStage>
       {showUnity ? (
-        <Unity
-          style={{ width: 1160, height: 680 }}
-          unityProvider={unityProvider}
-        />
+        <>
+          <LoadingBar
+            hidden={loadingProgression === 1}
+            style={{ display: loadingProgression === 1 ? "none" : "flex" }}
+          >
+            Loading... <br /> {Math.round(loadingProgression * 100)}%
+          </LoadingBar>
+
+          <Unity
+            hidden={loadingProgression !== 1}
+            style={{
+              width: 1160,
+              height: 680,
+              display: loadingProgression !== 1 ? "none" : "flex",
+            }}
+            unityProvider={unityProvider}
+          />
+        </>
       ) : (
         <PlayButton onClick={() => setShowUnity(true)}>Play</PlayButton>
       )}
-    </div>
+    </GameStage>
   );
 }
+
+const GameStage = styled.div`
+  & {
+    background-color: gray;
+    width: 1160px;
+    height: 680px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const LoadingBar = styled.div`
+  & {
+    display: inline-block;
+    text-decoration: none;
+    font-size: 2rem;
+    text-align: center;
+    transition: background 250ms ease-in-out, transform 150ms ease;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    padding: 1rem 2rem;
+    margin: 0;
+    width: 20rem;
+    justify-content: center;
+    align-items: center;
+  }
+`;
 
 const PlayButton = styled.button`
   & {
