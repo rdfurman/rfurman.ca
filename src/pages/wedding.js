023d1cd0ce677bde5d7f38b56371f85b821c8840
RDFurman <randydfurman@gmail.com>
2024-04-24 23:25:35 -0600

    Add basic wedding page

--- src/pages/wedding.js
+++ src/pages/wedding.js
@@ -1,31 +1,58 @@
-import { graphql } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 
-const WeddingPage = ({ data }) => {
+const WeddingPage = () => {
   return (
-    <div>
-      <h1>Furman Wedding</h1>
-      <h2>Make something nice here.</h2>
-      <p>To be made pretty.</p>
+    <div
+      style={{
+        display: "grid",
+        minHeight: "100vh",
+      }}
+    >
+      <StaticImage
+        style={{
+          // gridRow: 1,
+          gridArea: "1/1",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          // opacity: 0.9 /* Adjust opacity for better readability */,
+          zIndex: -1,
+        }}
+        layout="fullWidth"
+        alt=""
+        src="../images/wedding-bg.jpg"
+        quality="100"
+      />
+      <div
+        style={{
+          // gridRow: 1,
+          gridArea: "1/1",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "normal",
+          alignItems: "center",
+          color: "white" /* Adjust content color for visibility */,
+          padding: "2rem",
+        }}
+      >
+        <div
+          style={{
+            backgroundColor: "rgba(0, 0, 0, 0.8)",
+            paddingTop: "20px",
+            paddingLeft: "20px",
+            paddingRight: "20px",
+            textAlign: "center",
+          }}
+        >
+          <h1 style={{ fontWeight: "bolder", fontFamily: "cursive" }}>
+            We're Getting Hitched!
+          </h1>
+          <h3>2024-10-18</h3>
+          <p>Out in the bush.</p>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default WeddingPage;
-
-export const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-        pastJobTitle
-        pastEmployerLink
-        pastEmployer
-        description
-        currentJobTitle
-        currentEmployerLink
-        currentEmployer
-      }
-    }
-  }
-`;
